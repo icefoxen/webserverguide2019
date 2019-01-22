5373edb3ed940f9ce1592b744f6b40c305975a54
Simon Heath <icefoxen@gmail.com>
2019-01-22 17:14:14 -0500

    actix take 2

--- test_actix/src/main.rs
+++ test_actix/src/main.rs
@@ -1,12 +1,13 @@
-use actix_web::{server, App, HttpRequest, Responder};
-
-fn greet(_req: &HttpRequest) -> impl Responder {
-    format!("Hello world!")
-}
+use actix_web::{App, fs, server};
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(greet)))
+    server::new(|| App::new()
+        .handler(
+            "/static",
+            fs::StaticFiles::new(".")
+                .unwrap()
+                .show_files_listing()))
         .bind("127.0.0.1:8888")
         .expect("Can not bind to port 8888")
-        .run();
+        .run()
 }
